da758a18a39c0f0cb9dc8349e695c15d1d2eb0bd
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-03-13 17:59:25 +0900

    Redux TodoList 구현 및 최적화 (21.03.13)

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { decrease, increase, setDiff } from '../modules/counter';
 
 function CounterContainer() {
   // state는 store의 getState()할 때 넘어오는 그 state이다.
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual,
+  );
+  // const number = useSelector((state) => state.counter.number);
+  // const diff = useSelector((state) => state.counter.diff);
 
   // 액션을 dispatch 하기 위함
   const dispatch = useDispatch();
--- learn-redux/src/modules/todos.js
+++ learn-redux/src/modules/todos.js
@@ -2,7 +2,7 @@ const ADD_TODO = 'todos/ADD_TODO';
 const TOGGLE_TODO = 'todos/TOGGLE_TODO';
 
 let nextId = 1;
-export const addTODO = (text) => ({
+export const addTodo = (text) => ({
   type: ADD_TODO,
   todo: {
     id: nextId++,
