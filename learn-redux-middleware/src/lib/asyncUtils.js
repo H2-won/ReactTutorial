a705f1c8e3097aa03fe33f1f49e27bf14c1ba9d4
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-03-19 23:45:46 +0900

    asyncUtils를 통해 반복되는 코드 리팩토링 (21.03.19)

---
+++ learn-redux-middleware/src/lib/asyncUtils.js
@@ -0,0 +1,70 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (param) => async (dispatch) => {
+    dispatch({ type });
+    try {
+      // 모든 액션의 이름들을 payload로 통일시킨다.
+      const payload = await promiseCreator(param);
+      dispatch({
+        type: SUCCESS,
+        payload,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+      });
+    }
+  };
+};
+
+// 각 액션마다 posts, post 등 key로 받아옴
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
+
+export const reducerUtils = {
+  initial: (data) => ({
+    data,
+    loading: false,
+    error: null,
+  }),
+  loading: (prevState = null) => ({
+    data: prevState,
+    loading: true,
+    error: null,
+  }),
+  success: (data) => ({
+    data,
+    loading: false,
+    error: null,
+  }),
+  error: (error) => ({
+    data: null,
+    loading: false,
+    error,
+  }),
+};
--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,4 +1,9 @@
 import * as postsAPI from '../api/posts';
+import {
+  createPromiseThunk,
+  handleAsyncActions,
+  reducerUtils,
+} from '../lib/asyncUtils';
 
 // ducks 패턴 제대로 따를라면 'posts/GET_POSTS' 로 하는게 맞다
 
@@ -10,42 +15,29 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-export const getPosts = () => async (dispatch) => {
-  // 요청 시작
-  dispatch({ type: GET_POSTS });
-  // API 호출
-  try {
-    const posts = await postsAPI.getPosts();
-    // 성공
-    dispatch({
-      type: GET_POSTS_SUCCESS,
-      posts,
-    });
-  } catch (e) {
-    // 실패
-    dispatch({
-      type: GET_POSTS_ERROR,
-      error: e,
-    });
-  }
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
 };
 
-export const getPost = (id) => async (dispatch) => {
-  // 요청 시작
-  dispatch({ type: GET_POST });
-  // API 호출
-  try {
-    const post = await postsAPI.getPost(id);
-    // 성공
-    dispatch({
-      type: GET_POST_SUCCESS,
-      post,
-    });
-  } catch (e) {
-    // 실패
-    dispatch({
-      type: GET_POST_ERROR,
-      error: e,
-    });
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts');
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+    case GET_POSTS_SUCCESS:
+    case GET_POSTS_ERROR:
+      return getPostsReducer(state, action);
+    // 위 3개의 액션 중 하나라면, 해당 리듀서를 실행한다는 뜻
+    case GET_POST:
+    case GET_POST_SUCCESS:
+    case GET_POST_ERROR:
+      return getPostReducer(state, action);
+    // 위 3개의 액션 중 하나라면, 해당 리듀서를 실행한다는 뜻
+    default:
+      return state;
   }
-};
+}
