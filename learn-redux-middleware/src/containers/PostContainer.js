6175dfb6f9a08e3968ad462b4674125f509c9a12
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-03-23 18:05:41 +0900

    사용자 경험 개선을 위한 리팩토링 (21.03.23)
    재사용성을 높이고 불필요한 api 호출을 줄임

--- learn-redux-middleware/src/containers/PostContainer.js
+++ learn-redux-middleware/src/containers/PostContainer.js
@@ -1,21 +1,23 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { clearPost, getPost } from '../modules/posts';
+import { getPost } from '../modules/posts';
 import Post from '../components/Post';
+import { reducerUtils } from '../lib/asyncUtils';
 
 function PostContainer({ postId }) {
-  const { data, loading, error } = useSelector((state) => state.posts.post);
+  const { data, loading, error } = useSelector(
+    // || initial 한것은 초기값이 undefined 일 경우를 대비한 것이다. || {} 라고 해도됨
+    (state) => state.posts.post[postId] || reducerUtils.initial(),
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // 이미 data가 있다면 api 호출을 안함
+    if (data) return;
     dispatch(getPost(postId));
-    // return 부분 클리어 함수는 component가 unmount될때나, postId가 바뀌는 경우 윗 줄의 Effect 함수인 dispatch가 실행되기 전에 실행된다.
-    return () => {
-      dispatch(clearPost());
-    };
   }, [postId, dispatch]);
 
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
--- learn-redux-middleware/src/lib/asyncUtils.js
+++ learn-redux-middleware/src/lib/asyncUtils.js
@@ -20,6 +20,38 @@ export const createPromiseThunk = (type, promiseCreator) => {
   };
 };
 
+// idSelector는 함수형태로, api의 여러 파라미터 중 id를 선택하기 위한 것이다.
+// 지금은 파라미터가 id밖에 없어서 불필요하긴 하니까 기본 값 설정
+const defaultIdSelector = (param) => param;
+export const createPromiseThunkById = (
+  type,
+  promiseCreator,
+  idSelector = defaultIdSelector,
+) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (param) => async (dispatch) => {
+    const id = idSelector(param);
+    dispatch({ type, meta: id });
+    try {
+      // 모든 액션의 이름들을 payload로 통일시킨다.
+      const payload = await promiseCreator(param);
+      dispatch({
+        type: SUCCESS,
+        payload,
+        meta: id,
+      }); // 성공
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+        meta: id,
+      }); // 실패
+    }
+  };
+};
+
 // 각 액션마다 posts, post 등 key로 받아옴
 export const handleAsyncActions = (type, key, keepData) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
@@ -39,6 +71,43 @@ export const handleAsyncActions = (type, key, keepData) => {
         return {
           ...state,
           [key]: reducerUtils.error(action.error),
+        };
+      default:
+        return state;
+    }
+  };
+};
+
+export const handleAsyncActionsById = (type, key, keepData) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state, action) => {
+    const id = action.meta;
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.loading(
+              keepData ? state[key][id] && state[key][id].data : null,
+            ),
+          },
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.success(action.payload),
+          },
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: {
+            ...state[key],
+            [id]: reducerUtils.error(action.payload),
+          },
         };
       default:
         return state;
