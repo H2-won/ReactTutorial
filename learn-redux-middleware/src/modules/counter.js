469882473cacfc4c13eac0f71493e4adc438d60e
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-03-25 23:54:56 +0900

    Redux-saga 설치 및 counter,index 모듈 (21.03.25)

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,22 +1,45 @@
+import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
+const INCREASE_ASYNC = 'INCREASE_ASYNC';
+const DECREASE_ASYNC = 'DECREASE_ASYNC';
 
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+function* increaseSaga() {}
+
 // 정확히 따지자면 dispatch => 여기부터 thunk 함수이다. 전체는 thunk함수 크리에이터이다.
 // 1초 딜레이
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
+// export const increaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(increase());
+//   }, 1000);
+// };
 
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+// export const decreaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(decrease());
+//   }, 1000);
+// };
+
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
+
+function* increaseSaga() {
+  yield delay(1000); // 1초 기다림
+  yield put(increase()); // put은 특정 액션을 디스패치 해준다.
+}
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* counterSaga() {
+  yield takeEvery(INCREASE_ASYNC, increaseSaga); // 모든 INCREASE_ASYNC 액션을 처리한다.
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga); // 가장 마지막으로 디스패치된 DECREASE_ASYNC 액션만 처리한다.
+}
 
 const initialState = 0;
 
--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import posts from './posts';
+import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({ counter, posts });
+export function* rootSaga() {
+  yield all([counterSaga()]); // all = 배열 안의 saga들을 동시에 실행시켜준다.
+}
 
 export default rootReducer;
